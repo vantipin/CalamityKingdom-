4c315b75219a14e90e2bec97bfb458640c8b4f89
JerkUp <jerkupp@gmail.com>
2019-01-29 16:48:55 +0300

    Google base model reimplemented

--- Calamity/Models/GoogleBaseModel.swift
+++ Calamity/Models/GoogleBaseModel.swift
@@ -9,6 +9,22 @@
 import UIKit
 import Mantle
 
-class GoogleBaseModel: UIViewController {
+class GoogleBaseModel: GDBModel {
     var identifier: String = "";
+    
+    class func withoutNil() -> Bool {
+        return true
+    }
+    
+    func jsonKeyPathsByPropertyKey() -> [AnyHashable : Any]! {
+        return ["identifier" : "id"]
+    }
+    
+    func dictionaryValue() -> [AnyHashable : Any]! {
+        if let keyPaths = self.jsonKeyPathsByPropertyKey() {
+            return self.dictionaryWithValues(forKeys: (Array(keyPaths.keys) as! [String]))
+        }
+        
+        return [:]
+    }
 }
